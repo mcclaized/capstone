f244844a25d37aa2348fc9f7207f3406f40bab72
Steve McClain <sdmcclain@gmail.com>
2019-03-16 17:24:38 -0400

    improve CLI

--- Recommendation/main.py
+++ Recommendation/main.py
@@ -27,7 +27,7 @@ def get_similar_bonds(isin, features=None, cohort_attributes=None):
     
     if features is None:
         features = [col for col in bond_cohort.columns if col not in cohort_attributes]
-   
+  
     # Fit the model
     model = similar_bonds_pipeline()
     model.fit(bond_cohort[features])
@@ -46,15 +46,27 @@ def get_similar_bonds(isin, features=None, cohort_attributes=None):
 if __name__ == '__main__':
     from tabulate import tabulate
 
-    cohort_attributes = ['BCLASS3', 'Country', 'Ticker', 'Class - Detail - Code']
+    ## COLUMNS WHOSE VALUES MUST BE THE SAME IN ORDER TO BE CONSIDERED SIMILAR
+    #cohort_attributes = ['BCLASS3', 'Country', 'Ticker', 'Class - Detail - Code']
+    cohort_attributes = None
+
+    ## COLUMNS THAT THE MODEL SHOULD CONSIDER WHEN LOOKING FOR SIMILAR BONDS
     features = ["OAS", "OAD", "KRD 5Y", "KRD 10Y", "KRD 20Y", "KRD 30Y"]
+
+    ## COLUMNS TO DISPLAY IN THE CLI OUTPUT
+    display_columns = ['Ticker', 'BCLASS3', 'Country']  + (features or [])
 
     while True:
         isin = input("\n\nPlease Enter an ISIN: ")
-        bond = data_loader.get_bond(isin)
-        bond_table = tabulate(bond[cohort_attributes + features], headers='keys', tablefmt='psql')
+        try:
+            bond = data_loader.get_bond(isin)
+        except KeyError:
+            print("Invalid ISIN! Please try again.")
+            continue
+        bond_table = tabulate(bond[display_columns], headers='keys', tablefmt='psql')
         print("\nSearching for bonds that are similar to these characteristics:\n{}".format(bond_table))
+
         similar_bonds = get_similar_bonds(isin, features=features, cohort_attributes=cohort_attributes)
-        similar_bonds_table = tabulate(similar_bonds[cohort_attributes + features], headers='keys', tablefmt='psql')
+        similar_bonds_table = tabulate(similar_bonds[display_columns], headers='keys', tablefmt='psql')
         print("\nHere are your similar bonds!\n{}\n".format(similar_bonds_table))
 
